fd7a3ab539e2be09f6f062ac62e3f5045a46537f
AbdelrhmanSror <sror12347@gmail.com>
2019-12-29 19:21:08 +0200

    adding strategy pattern

--- src/strategyPattern/Duck.kt
+++ src/strategyPattern/Duck.kt
@@ -5,10 +5,14 @@ fun main() {
 
     /**
      * Design Principle:
+     *  Program to an interface, not an implementation and we did that when initializing the mallard duck below
+     *  <br></br>
+     *  Favor composition over inheritance. and we did that with flying and quaking behaviour ,we composited them in duck class
+     *  <br></br>
      * Identify the aspects of your application that vary and separate them from what stays the same.
      * and that is what we did with flying and quaking behaviour because not every type of duck can fly or can quake
      */
-    val duck = MallardDuck()
+    val duck: Duck = MallardDuck()
     duck.setFlyBehavior(FlyWithWings())
     duck.setQuackBehavior(Quack())
     duck.performQuake()
