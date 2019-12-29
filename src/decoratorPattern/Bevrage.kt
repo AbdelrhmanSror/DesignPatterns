dd1cfeaeca5f76d9319b036ff6c66364b33071fe
AbdelrhmanSror <sror12347@gmail.com>
2019-12-29 19:58:13 +0200

    adding decorator pattern

--- src/decoratorPattern/Bevrage.kt
+++ src/decoratorPattern/Bevrage.kt
@@ -3,7 +3,7 @@ package decoratorPattern
 fun main() {
     /**
      * Design Principle :
-     * Classes should be open for extension, but closed for modification. and that what decorator pattern do
+     * Classes should be open for extension, but closed for modification. and that what decorator pattern does
      */
     val espressoWithMocha = Mocha(Espresso())
     val houseBlendWithMocha = Mocha(Whip(HouseBlend()))
