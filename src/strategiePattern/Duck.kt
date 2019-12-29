439473b2814b311bcdd00070a8103b904d087b01
AbdelrhmanSror <sror12347@gmail.com>
2019-12-29 19:13:52 +0200

    adding strategy pattern

--- src/strategiePattern/Duck.kt
+++ src/strategiePattern/Duck.kt
@@ -2,6 +2,12 @@ package strategiePattern
 
 
 fun main() {
+
+    /**
+     * Design Principle:
+     * Identify the aspects of your application that vary and separate them from what stays the same.
+     * and that is what we did with flying and quaking behaviour because not every type of duck can fly or can quake
+     */
     val duck = MallardDuck()
     duck.setFlyBehavior(FlyWithWings())
     duck.setQuackBehavior(Quack())
@@ -9,7 +15,9 @@ fun main() {
     duck.performFlying()
 }
 
-
+/**
+ * here is the base class u can add behaviours to it during runtime including flying and quaking behaviour
+ */
 open class Duck {
     protected var flyBehaviour: FlyBehaviour? = null
     protected var quakeBehaviour: QuakeBehaviour? = null
